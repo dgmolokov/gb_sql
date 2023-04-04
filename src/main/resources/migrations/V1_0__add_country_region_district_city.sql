2ee0c045182316281ddda4089231f89a41abb24f
dgmolokov <dg.molokov@gmail.com>
2023-04-04 18:00:33 +0700

    fixed indents, changed varchar length, changed `id` type to "bigint"

--- src/main/resources/migrations/V1_0__add_country_region_district_city.sql
+++ src/main/resources/migrations/V1_0__add_country_region_district_city.sql
@@ -1,27 +1,27 @@
 create table if not exists `country` (
-    `id` int primary key unique not null auto_increment,
-    `name` varchar(20) unique not null
+  `id` int primary key unique not null auto_increment,
+  `name` varchar(50) unique not null
 );
 
 create table if not exists `region` (
-    `id` int primary key unique not null auto_increment,
-    `name` varchar(20) not null,
-    `country` int not null,
-    foreign key (`country`) references `country` (`id`)
+  `id` bigint primary key unique not null auto_increment,
+  `name` varchar(100) not null,
+  `country` int not null,
+  foreign key (`country`) references `country` (`id`)
 );
 
 create table if not exists `district` (
-    `id` int primary key unique not null auto_increment,
-    `name` varchar(20) unique not null,
-    `region` int not null,
-    foreign key (`region`) references `region` (`id`)
+  `id` bigint primary key unique not null auto_increment,
+  `name` varchar(100) unique not null,
+  `region` int not null,
+  foreign key (`region`) references `region` (`id`)
 );
 
 create table if not exists `city` (
-	`id` int primary key unique not null auto_increment,
-	`name` varchar(20) not null,
-	`region` int not null,
-	`district` int not null,
-	foreign key (`district`) references `district` (`id`),
-	foreign key (`region`) references `region` (`id`)
+  `id` bigint primary key unique not null auto_increment,
+  `name` varchar(50) not null,
+  `region` int not null,
+  `district` int not null,
+  foreign key (`district`) references `district` (`id`),
+  foreign key (`region`) references `region` (`id`)
 );
