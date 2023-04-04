120c7823484cf3e4e580b14597c48b5f19496f3e
dgmolokov <dg.molokov@gmail.com>
2023-04-04 17:42:58 +0700

    changed `ID` to `id`, varchar(45) to varchar(20), city field `district` to "not null"

--- src/main/resources/migrations/V1_0__add_country_region_district_city.sql
+++ src/main/resources/migrations/V1_0__add_country_region_district_city.sql
@@ -1,27 +1,27 @@
 create table if not exists `country` (
-    `ID` int primary key unique not null auto_increment,
-    `name` varchar(45) unique not null
+    `id` int primary key unique not null auto_increment,
+    `name` varchar(20) unique not null
 );
 
 create table if not exists `region` (
-    `ID` int primary key unique not null auto_increment,
-    `name` varchar(45) not null,
+    `id` int primary key unique not null auto_increment,
+    `name` varchar(20) not null,
     `country` int not null,
-    foreign key (`country`) references `country` (`ID`)
+    foreign key (`country`) references `country` (`id`)
 );
 
 create table if not exists `district` (
-    `ID` int primary key unique not null auto_increment,
-    `name` varchar(45) unique not null,
+    `id` int primary key unique not null auto_increment,
+    `name` varchar(20) unique not null,
     `region` int not null,
-    foreign key (`region`) references `region` (`ID`)
+    foreign key (`region`) references `region` (`id`)
 );
 
 create table if not exists `city` (
-	`ID` int primary key unique not null auto_increment,
-	`name` varchar(45) not null,
+	`id` int primary key unique not null auto_increment,
+	`name` varchar(20) not null,
 	`region` int not null,
-	`district` int default null,
-	foreign key (`district`) references `district` (`ID`),
-	foreign key (`region`) references `region` (`ID`)
+	`district` int not null,
+	foreign key (`district`) references `district` (`id`),
+	foreign key (`region`) references `region` (`id`)
 );
