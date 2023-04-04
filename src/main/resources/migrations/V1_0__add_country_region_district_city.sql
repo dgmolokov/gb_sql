bea7b685c995014fec3e629a2f64b99d33d44a19
dgmolokov <dg.molokov@gmail.com>
2023-04-04 18:09:20 +0700

    changed varchars length

--- src/main/resources/migrations/V1_0__add_country_region_district_city.sql
+++ src/main/resources/migrations/V1_0__add_country_region_district_city.sql
@@ -1,25 +1,25 @@
 create table if not exists `country` (
   `id` bigint primary key unique not null auto_increment,
-  `name` varchar(50) unique not null
+  `name` varchar(100) unique not null
 );
 
 create table if not exists `region` (
   `id` bigint primary key unique not null auto_increment,
-  `name` varchar(100) not null,
+  `name` varchar(200) not null,
   `country` bigint not null,
   foreign key (`country`) references `country` (`id`)
 );
 
 create table if not exists `district` (
   `id` bigint primary key unique not null auto_increment,
-  `name` varchar(100) unique not null,
+  `name` varchar(200) unique not null,
   `region` bigint not null,
   foreign key (`region`) references `region` (`id`)
 );
 
 create table if not exists `city` (
   `id` bigint primary key unique not null auto_increment,
-  `name` varchar(50) not null,
+  `name` varchar(200) not null,
   `region` bigint not null,
   `district` bigint not null,
   foreign key (`district`) references `district` (`id`),
