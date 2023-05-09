5eca15262d7a5dadec2cda058dda0db8caf164e6
dgmolokov <dg.molokov@gmail.com>
2023-05-09 18:28:29 +0700

    fix indents, remade trigger add_welcome_bonus

--- src/main/resources/migrations/V4_3__employees_welcome_bonus_trigger.sql
+++ src/main/resources/migrations/V4_3__employees_welcome_bonus_trigger.sql
@@ -1,8 +1,13 @@
 delimiter //
-  create trigger add_welcome_bonus
-    after insert
-    on employees
-    for each row
-    insert into salaries(emp_no, salary, from_date, to_date)
-    values(new.emp_no, 100000, curdate(), curdate()) //
-delimiter ;
+create trigger add_welcome_bonus
+  after insert
+  on salaries
+  for each row
+begin
+  declare salaries_count int;
+  set salaries_count = (select count(*) from salaries where emp_no = new.emp_no);
+  if salaries_count = 0 then
+    insert into salaries (emp_no, salary, from_date, to_date)
+    values (new.emp_no, new.salary * 0.5, curdate(), curdate());
+  end if;
+end //
